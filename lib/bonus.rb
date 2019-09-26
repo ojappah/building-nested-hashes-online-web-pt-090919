fb2dadc04a2552385b244ce4c8c185f989edaffe
generous-vector-9581 <ojappah@gmail.com>
2019-09-26 23:26:40 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,6 @@ def bonus
    }
   }
 
-epic_tragedy[:montague][:patriarch][:hero][:status] = "dead"
-epic_tragedy[:capulet][:patriarch][:heroine][:status] = "dead"
-  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
